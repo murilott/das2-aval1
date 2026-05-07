e254f399590b4303cc498a4268cca54aad5043ab
murilott <murilo_teichert@hotmail.com>
2026-05-07 19:49:57 -0300

    feat: adicionado comando que registra functions

--- src/function_app.py
+++ src/function_app.py
@@ -3,6 +3,12 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
+# importação das functions
 from triggers.extract_cliente import app as extract_cliente
 from triggers.extract_pedido import app as extract_pedido
 from triggers.extract_produto import app as extract_produto
+
+# registrar e rodar functions
+app.register_functions(extract_cliente)
+app.register_functions(extract_pedido)
+app.register_functions(extract_produto)
